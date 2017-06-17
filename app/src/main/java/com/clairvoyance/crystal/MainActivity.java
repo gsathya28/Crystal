7721d367d60f313c00391c06ab2dbe6037762bcd
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-06-17 17:51:20 -0500

    We have navigation bar complete, currently formatting the TextViews when they are generated (also need to fix the problem regarding clicking the same navigation button when it is active. Everything re-generates: needs conditional)

--- app/src/main/java/com/clairvoyance/crystal/MainActivity.java
+++ app/src/main/java/com/clairvoyance/crystal/MainActivity.java
@@ -1,10 +1,14 @@
 package com.clairvoyance.crystal;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +21,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_agenda:
+                    mTextMessage.setText("Agenda");
+                    createAgenda();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_weekly:
+                    mTextMessage.setText("Weekly");
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_monthly:
+                    mTextMessage.setText("Monthly");
                     return true;
             }
             return false;
@@ -40,6 +45,42 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_agenda);
+    }
+
+    private void createAgenda()
+    {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mainLayout);
+
+        String[] textArray = {"One", "Two", "Three", "Four"};
+
+        for( int i = 0; i < textArray.length; i++ )
+        {
+            TextView textView = new TextView(this);
+            textView.setText(textArray[i]);
+            // Formatting the TextView
+            /*
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+
+
+            Resources r = mContext.getResources();
+            int topbottomPx = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    R.dimen.activity_vertical_margin,
+                    r.getDisplayMetrics()
+            );
+            params.setMargins(R.dimen.activity_horizontal_margin, R.dimen.activity_vertical_margin, R.dimen.activity_horizontal_margin, R.dimen.activity_vertical_margin);
+
+            textView.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
+            textView.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+
+            */
+
+            linearLayout.addView(textView);
+        }
     }
 
 }
