9fe6ce44346e99142bda211f110e7c1d120413bf
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-08-15 15:19:34 -0500

    Delete Functionality works!

--- app/src/main/java/com/clairvoyance/crystal/ViewEvent.java
+++ app/src/main/java/com/clairvoyance/crystal/ViewEvent.java
@@ -1,7 +1,10 @@
 package com.clairvoyance.crystal;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +25,9 @@ public class ViewEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_event);
+
+        final CrystalCalendar localCalendar = new CrystalCalendar(Build.ID + "@clairvoyance.com");
+        CrystalCalendar.read(ViewEvent.this, localCalendar);
 
         Intent viewEventIntent = getIntent();
         event = (CrystalEvent) viewEventIntent.getSerializableExtra("Event");
@@ -58,6 +64,32 @@ public class ViewEvent extends AppCompatActivity {
                 Intent editEventIntent = new Intent(v.getContext(), EditEvent.class);
                 editEventIntent.putExtra("Event", event);
                 startActivity(editEventIntent);
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ViewEvent.this);
+                builder.setMessage("Are you sure you want to delete the event: " + event.get(CrystalEvent.NAME) + "?");
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        localCalendar.remove(event);
+                        Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(mainActivityIntent);
+                        localCalendar.save(ViewEvent.this);
+                    }
+                });
+
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing
+                    }
+                });
+                builder.create().show();
+
             }
         });
 
