3ed61c00fa839231eaa73bc9cca070af314f7209
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-10-14 14:36:24 -0500

    CrystalActivity Abstract Class

---
+++ app/src/main/java/com/clairvoyance/crystal/CrystalActivity.java
@@ -0,0 +1,11 @@
+package com.clairvoyance.crystal;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sathya on 10/14/2017.
+ */
+
+public abstract class CrystalActivity extends AppCompatActivity {
+
+}
